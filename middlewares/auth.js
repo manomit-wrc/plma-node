364500fff6add3e335d43d5ddbce21f195a47ff5
Manomit Mitra <manomit@wrctechnologies.com>
2018-06-14 12:25:59 +0530

    commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,9 @@
 module.exports = (req, res, next) => {
     if(req.isAuthenticated()) {
+        const ulrArr = req.originalUrl.split("/");
         delete req.user.password;
         res.locals.user = req.user;
-        res.locals.client_url = req.originalUrl.substr(1);
+        res.locals.client_url = ulrArr[1];
         return next();
     }
     res.redirect('/');
--- routes/employee.js
+++ routes/employee.js
@@ -13,8 +13,26 @@ var csrfProtection = csrf({ cookie: true });
 
 router.get('/employees', auth, (req, res) => {
     res.render('employees/index', { layout: 'dashboard' });
-}).get('/employees/add', csrfProtection, auth, (req, res) => {
-    res.render('employees/add', { layout: 'dashboard', csrfToken: req.csrfToken() });
+}).get('/employees/add', csrfProtection, auth, async (req, res) => {
+    var success_message = req.flash('success-message')[0];
+    var error_message = req.flash('error-message')[0];
+    const firms = await Firm.findAll({});
+    res.render('employees/add', { layout: 'dashboard', csrfToken: req.csrfToken(), firms, success_message,error_message  });
+});
+
+router.post('/employees/add', auth, csrfProtection, async (req, res) => {
+    const data = await User.findAll({
+        where: {
+            email: req.body.email
+        }
+    });
+    if(data.length === 0) {
+
+    }
+    else {
+        req.flash('error-message', 'Email already taken.');
+        res.redirect('/employees/add');
+    }
 });
 
 module.exports = router;
