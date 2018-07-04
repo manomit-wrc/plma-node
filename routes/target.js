437bec52a51bbc50627ce16b5de7370337730c91
Sayan Sadhu <sayan@wrctechnologies.com>
2018-07-04 17:05:15 +0530

    Commited by Sayan

--- routes/target.js
+++ routes/target.js
@@ -8,6 +8,8 @@ const bCrypt = require('bcrypt-nodejs');
 const designation = require('../models').designation;
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
+const excel = require('node-excel-export');
+const multer = require('multer');
 const PracticeArea = require('../models').practicearea;
 const Section = require('../models').section;
 const Group = require('../models').group;
@@ -23,39 +25,53 @@ const Jurisdiction = require('../models').jurisdiction;
 const user = require('../models').user;
 var csrfProtection = csrf({ cookie: true });
 var csv = require('fast-csv');
-var path      = require('path');
+var path = require('path');
 var fs = require('fs');
 const router = express.Router();
 
+var fileExt = '';
+var fileName = '';
 
-function removePhoneMask (phone_no){
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/target');
+    },
+    filename: function (req, file, cb) {
+      fileExt = file.originalname.split('.')[1];
+      fileName = req.user.id + '-' + Date.now() + '.' + fileExt;
+      cb(null, fileName);
+    }
+})
+
+var upload = multer({ storage: storage });
+
+function removePhoneMask(phone_no) {
 	var phone_no = phone_no.replace("-","");
 	phone_no = phone_no.replace(")","");
 	phone_no = phone_no.replace("(","");
 	phone_no = phone_no.replace(" ","");
 	return phone_no;
-
 }
 
-router.get('/target',auth, firmAttrAuth, csrfProtection, (req,res) => {
+router.get('/target', auth, firmAttrAuth, csrfProtection, (req, res) => {
 	var success_message = req.flash('success-message')[0];
 	var success_edit_message = req.flash('success-edit-message')[0];
 	var whereCondition = {};
-	if(req.query.searchName) {
+	if (req.query.searchName) {
 		whereCondition.target_type = req.query.searchName;
 	}
-	if(req.query.searchEmail) {
+	if (req.query.searchEmail) {
 		whereCondition.email = req.query.searchEmail;
 	}
 	whereCondition.firm_id = req.user.firm_id.toString();
-	if(req.user.role_id != 2)
-	{
+	if (req.user.role_id != 2) {
 		whereCondition.user_id = req.user.id;
 	}
 	Target.findAll({
 		where: whereCondition
 	}).then(targets => {
-		res.render('target/targets',{ 
+		res.render('target/targets', { 
 			layout: 'dashboard',  
 			csrfToken: req.csrfToken(),
 			targets: targets, 
@@ -67,12 +83,11 @@ router.get('/target',auth, firmAttrAuth, csrfProtection, (req,res) => {
 	});
 });
 
-router.get('/target/add',auth, firmAttrAuth, csrfProtection, (req,res) => {
+router.get('/target/add', auth, firmAttrAuth, csrfProtection, (req, res) => {
 	var error_message = req.flash('error-target-message')[0];
 	designation.findAll().then(designation => {
 		industry_type.findAll().then(industry => {
 			Country.findAll().then(country => {
-
 				State.findAll().then(state => {
 					res.render('target/addtarget',{ layout: 'dashboard', csrfToken: req.csrfToken(), country: country, state: state, designations: designation, industry: industry, error_message  });
 				});
@@ -81,17 +96,15 @@ router.get('/target/add',auth, firmAttrAuth, csrfProtection, (req,res) => {
 	});
 });
 
-router.post('/target/addtarget',auth, firmAttrAuth,csrfProtection, async (req,res) => {
+router.post('/target/addtarget', auth, firmAttrAuth, csrfProtection, async (req, res) => {
 	const formatDate = req.body.target_dob ? req.body.target_dob.split("-") : '';
 	const target_data = await Target.findOne({
 		where: {
-            email: req.body.email
-        }
+			email: req.body.email
+		}
 	});
-	if(target_data === null)
-	{
-		if(req.body.target_type === "O")
-		{
+	if (target_data === null) {
+		if (req.body.target_type === "O") {
 			const store_org_data = await Target.create({
 				organization_name: req.body.org_name,
 				organization_id: req.body.org_id,
@@ -122,10 +135,8 @@ router.post('/target/addtarget',auth, firmAttrAuth,csrfProtection, async (req,re
 				remarks: req.body.remarks
 			});
 			req.flash('success-message', 'Target Added Successfully');
-            res.redirect('/target')
-		}
-		else
-		{
+			res.redirect('/target')
+		} else {
 			const store_ind_data = await Target.create({
 				first_name: req.body.target_first_name,
 				last_name: req.body.target_last_name,
@@ -159,24 +170,22 @@ router.post('/target/addtarget',auth, firmAttrAuth,csrfProtection, async (req,re
 				remarks: req.body.remarks
 			});
 			req.flash('success-message', 'Target Added Successfully');
-            res.redirect('/target')
+			res.redirect('/target')
 		}
-	}
-	else
-	{
+	} else {
 		req.flash('error-target-message', 'Email already taken.');
-        res.redirect('/target/add');
+		res.redirect('/target/add');
 	}
 });
 
-router.get('/target/edit/:id',auth, firmAttrAuth, csrfProtection, async(req,res) => {
+router.get('/target/edit/:id', auth, firmAttrAuth, csrfProtection, async (req, res) => {
 	var error_message = req.flash('error-clientEdit-message')[0];
 	const clients = await Target.findById(req.params['id']);
 	const designations = await designation.findAll();
 	const industrys = await industry_type.findAll();
 	const client_country = await Country.findAll();
 	const client_state = await State.findAll({
-		where: {country_id : "233"}
+		where: { country_id : "233" }
 	});
 	const client_city = await City.findAll({
 		where: {
@@ -189,22 +198,20 @@ router.get('/target/edit/:id',auth, firmAttrAuth, csrfProtection, async(req,res)
 			city_name: client_cities.name
 		}
 	});
-
-	res.render('target/targetupdate',{ layout: 'dashboard', csrfToken: req.csrfToken(),designation: designations,industry: industrys,client:clients,country: client_country, state: client_state, city: client_city, zipcode: client_zipcode, error_message });
+	res.render('target/targetupdate', { layout: 'dashboard', csrfToken: req.csrfToken(), designation: designations, industry: industrys, client:clients, country: client_country, state: client_state, city: client_city, zipcode: client_zipcode, error_message });
 });
 
-router.post('/target/edittarget/:id',auth, firmAttrAuth,csrfProtection, async (req,res) => {
+router.post('/target/edittarget/:id', auth, firmAttrAuth, csrfProtection, async (req, res) => {
 	const formatDate = req.body.client_dob ? req.body.client_dob.split("-") : '';
 	const target_edit_data = await Target.findOne({
-        where: {
-            email: req.body.email,
-            id: {
-                [Op.ne]: req.params['id']
-            }
-        }
-    });
-	if(target_edit_data === null)
-	{
+		where: {
+			email: req.body.email,
+			id: {
+				[Op.ne]: req.params['id']
+			}
+		}
+	});
+	if (target_edit_data === null) {
 		const edit_client = await Target.update({
 			organization_name: req.body.org_name,
 			organization_id: req.body.org_id,
@@ -237,27 +244,29 @@ router.post('/target/edittarget/:id',auth, firmAttrAuth,csrfProtection, async (r
 			target_id : req.body.client_id,
 			target_code : req.body.master_id,
 			remarks: req.body.remarks
-		},{where: {id: req.params['id']}
-		});
+		}, {where: {id: req.params['id']}
+	});
 		req.flash('success-edit-message', 'Client Updated Successfully');
-        res.redirect('/target')
-	} 
-	else
-	{
+		res.redirect('/target')
+	} else {
 		req.flash('error-clientEdit-message', 'Email already taken.');
-        res.redirect('/target/edit/'+req.params['id']);
+		res.redirect('/target/edit/'+req.params['id']);
 	}
 });
 
-router.get('/target/delete/:id',auth, firmAttrAuth, (req,res) => {
-  Target.destroy({
-    where:{
-      id: req.params.id
-    }
-  }).then(resp => {
+router.get('/target/delete/:id', auth, firmAttrAuth, (req, res) => {
+	Target.destroy({
+		where: {
+			id: req.params.id
+		}
+	}).then(resp => {
 		// req.flash('success_delete_attorney','Attorney deleted successfully');
-    res.redirect('/target');
-  });
+		res.redirect('/target');
+	});
+});
+
+router.post('/target/import', auth, upload.single('file_name'), csrfProtection, (req, res) => {
+	console.log('target uploaded successfully');
 });
 
 module.exports = router;
