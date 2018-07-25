b07207b37a087bc7d5b492bb87c083d76f45ca5c
malini <roychowdhury.malini@gmail.com>
2018-07-25 17:16:06 +0530

    malini

--- models/activity.js
+++ models/activity.js
@@ -1,12 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var activity = sequelize.define('activity', {
-    firm: DataTypes.STRING,
+    firm_id: DataTypes.INTEGER,
     activity_type: DataTypes.STRING,
     activity_goal_id: DataTypes.INTEGER,
-    practice_area: DataTypes.STRING,
+    practice_area: DataTypes.INTEGER,
     potiential_revenue: DataTypes.STRING,
-    remarks: DataTypes.STRING,
+    remarks: DataTypes.TEXT,
     activity_creation_date: DataTypes.DATE,
     activity_from_date: DataTypes.DATE,
     activity_to_date: DataTypes.DATE,
@@ -18,7 +18,9 @@ module.exports = (sequelize, DataTypes) => {
     total_budget_hour: DataTypes.STRING,
     total_budget_amount: DataTypes.STRING,
     user_id: DataTypes.INTEGER,
-    clint: DataTypes.STRING
+    section_id: DataTypes.INTEGER,
+    s_group_id: DataTypes.STRING
+
   }, {});
   activity.associate = function(models) {
     // associations can be defined here
