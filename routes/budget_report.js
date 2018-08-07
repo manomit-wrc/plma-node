02d24043c609c63d6f7f46082dfd5ba0886ec157
Siddhartha Mukherjee <siddhartha@wrctpl.com>
2018-08-07 17:04:58 +0530

    Commit by Siddhartha

--- routes/budget_report.js
+++ routes/budget_report.js
@@ -148,7 +148,8 @@ router.get('/activity-budget-report', auth, csrfProtection, async (req, res) => 
 });
 
 router.get('/budget-report/activity-goal/:id', auth, csrfProtection, async (req, res) => {
-
+  console.log("-------------------------------------------------------");
+  
   var user_id = req.user.id;
   Activity.hasMany(ActivityBudget, {
     foreignKey: 'activity_id'
