478ccb43f088332c8c90a339f78139be387e9806
Bratin Meheta <bratin@wrctpl.com>
2018-08-08 14:43:02 +0530

    committed by bratin

--- routes/activity_goal.js
+++ routes/activity_goal.js
@@ -72,6 +72,7 @@ router.post('/activity-goal/add', auth, firmAttrAuth, csrfProtection, (req, res)
 		firm_id: req.user.firm_id,
 		user_id: req.user.id,
 		activity_goal: req.body.activity_goal,
+		category: req.body.category,
 		from_date: formatFromDate ? formatFromDate[2]+"-"+formatFromDate[1]+"-"+formatFromDate[0] : null,
 		to_date: formatToDate ? formatToDate[2]+"-"+formatToDate[1]+"-"+formatToDate[0] : null,
 		remarks: req.body.remarks
@@ -100,6 +101,7 @@ router.post('/activity-goal/edit/:id', auth, firmAttrAuth, csrfProtection, (req,
 	const formatToDate1 = req.body.to_date ? req.body.to_date.split("-") : '';
 	ActivityGoal.update({
 		activity_goal: req.body.activity_goal,
+		category: req.body.category,
 		from_date: formatFromDate1 ? formatFromDate1[2]+"-"+formatFromDate1[1]+"-"+formatFromDate1[0] : null,
 		to_date: formatToDate1 ? formatToDate1[2]+"-"+formatToDate1[1]+"-"+formatToDate1[0] : null,
 		remarks: req.body.remarks
