630fd7c7c144ec0332bf6288cbea34d93e7faa39
Sayan Sadhu <sayan@wrctechnologies.com>
2018-08-21 17:31:41 +0530

    Commited by Sayan

--- routes/activity_goal.js
+++ routes/activity_goal.js
@@ -2,17 +2,9 @@ const express = require('express');
 const auth = require('../middlewares/auth');
 const firmAttrAuth = require('../middlewares/firm_attr_auth');
 const csrf = require('csurf');
-const bCrypt = require('bcrypt-nodejs');
-const gravatar = require('gravatar');
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
-// const dateFormat = require('dateformat');
-const User = require('../models').user;
 const Firm = require('../models').firm;
 const ActivityGoal = require('../models').activity_goal;
-
-var csrfProtection = csrf({ cookie: true });
-
+const csrfProtection = csrf({ cookie: true });
 const router = express.Router();
 
 /*==================================Bratin Meheta 15-06-2018===================================*/
@@ -24,28 +16,26 @@ router.get('/activity-goal', auth, firmAttrAuth, csrfProtection, (req, res) => {
 	const formatFromDate2 = req.query.from_activity_goal_date ? req.query.from_activity_goal_date.split("-") : '';
 	const formatToDate2 = req.query.to_activity_goal_date ? req.query.to_activity_goal_date.split("-") : '';
 	var whereGoals = {};
-	if(formatFromDate2)
-	{
-		whereGoals.from_date = formatFromDate2[2]+"-"+formatFromDate2[1]+"-"+formatFromDate2[0];
+	if (formatFromDate2) {
+		whereGoals.from_date = formatFromDate2[2] + "-" + formatFromDate2[1] + "-" + formatFromDate2[0];
 	}
-	if(formatToDate2)
-	{
-		whereGoals.to_date = formatToDate2[2]+"-"+formatToDate2[1]+"-"+formatToDate2[0];
+	if (formatToDate2) {
+		whereGoals.to_date = formatToDate2[2] + "-" + formatToDate2[1] + "-" + formatToDate2[0];
 	}
 	whereGoals.firm_id = req.user.firm_id;
-	ActivityGoal.belongsTo(Firm, {foreignKey: 'firm_id'});
+	ActivityGoal.belongsTo(Firm, { foreignKey: 'firm_id' });
 	ActivityGoal.findAll({
 		where: whereGoals,
 		include: [{
-            model: Firm
-        }]
-	}).then(goals =>{
-	res.render('activity_goal/index', {
+			model: Firm
+		}]
+	}).then(goals => {
+		res.render('activity_goal/index', {
 			layout: 'dashboard',
-			success_message:success_message,
+			success_message: success_message,
 			success_edit_message: success_edit_message,
 			success_del_message: success_del_message,
-			goals:goals,
+			goals: goals,
 			from_activity_goal_date: req.query.from_activity_goal_date ? req.query.from_activity_goal_date : '',
 			to_activity_goal_date: req.query.to_activity_goal_date ? req.query.to_activity_goal_date : ''
 		});
@@ -53,7 +43,7 @@ router.get('/activity-goal', auth, firmAttrAuth, csrfProtection, (req, res) => {
 });
 
 router.get('/activity-goal/add', auth, firmAttrAuth, csrfProtection, (req, res) => {
-	res.render('activity_goal/add', { layout: 'dashboard', csrfToken: req.csrfToken()});
+	res.render('activity_goal/add', { layout: 'dashboard', csrfToken: req.csrfToken() });
 });
 
 router.post('/activity-goal/add', auth, firmAttrAuth, csrfProtection, (req, res) => {
@@ -64,10 +54,10 @@ router.post('/activity-goal/add', auth, firmAttrAuth, csrfProtection, (req, res)
 		user_id: req.user.id,
 		activity_goal: req.body.activity_goal,
 		category: req.body.category,
-		from_date: formatFromDate ? formatFromDate[2]+"-"+formatFromDate[1]+"-"+formatFromDate[0] : null,
-		to_date: formatToDate ? formatToDate[2]+"-"+formatToDate[1]+"-"+formatToDate[0] : null,
+		from_date: formatFromDate ? formatFromDate[2] + "-" + formatFromDate[1] + "-" + formatFromDate[0] : null,
+		to_date: formatToDate ? formatToDate[2] + "-" + formatToDate[1] + "-" + formatToDate[0] : null,
 		remarks: req.body.remarks
-	   }).then(store =>{
+	}).then(() => {
 		req.flash('success-activityGoal-message', 'Activity Goal Created Successfully');
 		res.redirect('/activity-goal');
 	});
@@ -75,17 +65,15 @@ router.post('/activity-goal/add', auth, firmAttrAuth, csrfProtection, (req, res)
 
 router.get('/activity-goal/edit/:id', auth, firmAttrAuth, csrfProtection, (req, res) => {
 	ActivityGoal.findById(req.params['id']).then(result => {
-		res.render('activity_goal/edit', { layout: 'dashboard', csrfToken: req.csrfToken(), edit_goals: result})
+		res.render('activity_goal/edit', { layout: 'dashboard', csrfToken: req.csrfToken(), edit_goals: result })
 	});
 });
-
 
 router.get('/activity-goal/view/:id', auth, firmAttrAuth, csrfProtection, (req, res) => {
 	ActivityGoal.findById(req.params['id']).then(result => {
-		res.render('activity_goal/view', { layout: 'dashboard', csrfToken: req.csrfToken(), edit_goals: result})
+		res.render('activity_goal/view', { layout: 'dashboard', csrfToken: req.csrfToken(), edit_goals: result })
 	});
 });
-
 
 router.post('/activity-goal/edit/:id', auth, firmAttrAuth, csrfProtection, (req, res) => {
 	const formatFromDate1 = req.body.from_date ? req.body.from_date.split("-") : '';
@@ -93,27 +81,29 @@ router.post('/activity-goal/edit/:id', auth, firmAttrAuth, csrfProtection, (req,
 	ActivityGoal.update({
 		activity_goal: req.body.activity_goal,
 		category: req.body.category,
-		from_date: formatFromDate1 ? formatFromDate1[2]+"-"+formatFromDate1[1]+"-"+formatFromDate1[0] : null,
-		to_date: formatToDate1 ? formatToDate1[2]+"-"+formatToDate1[1]+"-"+formatToDate1[0] : null,
+		from_date: formatFromDate1 ? formatFromDate1[2] + "-" + formatFromDate1[1] + "-" + formatFromDate1[0] : null,
+		to_date: formatToDate1 ? formatToDate1[2] + "-" + formatToDate1[1] + "-" + formatToDate1[0] : null,
 		remarks: req.body.remarks
-	},{where: {id: req.params['id']}
-	}).then(values => {
-		req.flash('success-edit-goal-message', 'Activity Goal Edited Successfully');
-    res.redirect('/activity-goal');
-	});
-});
-
-router.get('/activity-goal/delete/:id', auth, firmAttrAuth, csrfProtection, (req, res) =>{
-	ActivityGoal.destroy({
+	}, {
 		where: {
 			id: req.params['id']
 		}
-	}).then(result => {
-		req.flash('success-delete-goal-message', 'Activity Goal Delete Successfully');
+	}).then(() => {
+		req.flash('success-edit-goal-message', 'Activity Goal Updated Successfully');
 		res.redirect('/activity-goal');
 	});
 });
 
+router.get('/activity-goal/delete/:id', auth, firmAttrAuth, csrfProtection, (req, res) => {
+	ActivityGoal.destroy({
+		where: {
+			id: req.params['id']
+		}
+	}).then(() => {
+		req.flash('success-delete-goal-message', 'Activity Goal Deleted Successfully');
+		res.redirect('/activity-goal');
+	});
+});
 
 /*==================================Bratin Meheta 15-06-2018===================================*/
 
